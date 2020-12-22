f19ec7df99ca4805712bc733c5cecff9748e7349
AsafBanana <asafba4@gmail.com>
2020-12-22 12:35:07 +0200

    Add files via upload

--- competing_risks_model.R
+++ competing_risks_model.R
@@ -11,24 +11,31 @@ library(rootSolve)
 library(doParallel)
 source("utils.R")
 
-
-N_QUESTION = 2
-N_EVENTS = 2
-N_P = 20
+#Compile and import c code
 sourceCpp("comp_risk.cpp", verbose=TRUE)
-
+N_P = 20
 CompetingRisksModel = setRefClass('CompetingRisksModel',
                                   fields = list(                                
                                       event_specific_models = "list",
 				      omega_hat = "matrix",
 				      omega_varcov_hat = "matrix",
 				      N = "numeric",
-				      N_QUESTION = "numeric",
-				      N_EVENTS = "numeric"
+				      N_CLUESTERS_SIZE = "numeric",
+				      N_FAILURE_TYPES = "numeric"
                                     )
                                   )
 # MAIN API:
 CompetingRisksModel$methods(
+
+  # This function runs the EM algotihem.
+  # It expects 
+  # t - a [n_clusters,n_cluster_size] matrix of observed failure times
+  # delta - a [n_clusters,n_cluster_size,n_failures] array of indicates if specific failture was observed.
+  # X - a [n_clusters,n_cluster_size,n_covariates] array of covarites.
+  # W - optional [n_clusters] array of weights.
+  # A - optional [n_clusters,n_cluster_size] matrix indicating if the specific memmber exists in the cluster. This allows handling missing memmbers. 
+  # tol - optional convergance threshold.
+  # MAX_ITR optional maximum number of iteration in attempt to reach convergance. 
   fit = function(t, 
                  delta, 
                  X,  
@@ -38,19 +45,19 @@ CompetingRisksModel$methods(
 		 MAX_ITR=150,ncore=1) {
 	registerDoParallel(cores=ncore)
 	N <<- dim(delta)[1]
-	N_QUESTION <<- dim(delta)[2]
-	N_EVENTS <<- dim(delta)[3]
+	N_CLUESTERS_SIZE <<- dim(delta)[2]
+	N_FAILURE_TYPES <<- dim(delta)[3]
 	if (is.null(W)) {
 		W = rep(1,N)
 	}
 
 	if (is.null(A)) {
-		A = matrix(rep(1,N*N_QUESTION),ncol=N_QUESTION)
+		A = matrix(rep(1,N*N_CLUESTERS_SIZE),ncol=N_CLUESTERS_SIZE)
 	}
 	delta_sums = apply(delta,3,rowSums)
 	conv=1
-	omega_hat <<- matrix(rep(1,N*N_EVENTS),ncol=N_EVENTS)
-	beta_hat = matrix(rep(0,N_QUESTION*N_EVENTS),ncol=N_EVENTS) 
+	omega_hat <<- matrix(rep(1,N*N_FAILURE_TYPES),ncol=N_FAILURE_TYPES)
+	beta_hat = matrix(rep(0,N_CLUESTERS_SIZE*N_FAILURE_TYPES),ncol=N_FAILURE_TYPES) 
 	omega_varcov_hat <<-  matrix(c(1,0,0,0,1,0,0,0,1),ncol=3)
 	k=0
 	while (conv > tol) {
@@ -66,9 +73,9 @@ CompetingRisksModel$methods(
 		cox_step(t,X,omega_hat,W,A)
 		beta_Z = get_beta_Z(X)
 		hazared_at_event = get_hazerd_at_event(t,delta,X,A,W)
-		gh = mgauss.hermite(N_P,rep(0,N_EVENTS),omega_varcov_hat)
+		gh = mgauss.hermite(N_P,rep(0,N_FAILURE_TYPES),omega_varcov_hat)
 		omega_varcov_hat_tmp = E_phase_C(delta_sums,gh$points,gh$weights,flatten_array(hazared_at_event),flatten_array(beta_Z))
-		gh = mgauss.hermite(N_P,rep(0,N_EVENTS),omega_varcov_hat)
+		gh = mgauss.hermite(N_P,rep(0,N_FAILURE_TYPES),omega_varcov_hat)
 		omega_hat <<- omega_expectation_C(delta_sums,gh$points,gh$weights,flatten_array(hazared_at_event),flatten_array(beta_Z))
 		omega_varcov_hat <<- omega_varcov_hat_tmp
 		conv = sum(abs(old_beta_hat - beta_hat)) + sum(abs(old_omega_varcov_hat - omega_varcov_hat))
@@ -81,9 +88,9 @@ CompetingRisksModel$methods(
 
   cox_step = function(t,X,O,W,A) {
 		ret = list()
-		Q_res <- foreach (i=1:N_QUESTION)  %dopar% {
+		Q_res <- foreach (i=1:N_CLUESTERS_SIZE)  %dopar% {
 			ret = list()
-			for (j in seq(1,N_EVENTS)) {
+			for (j in seq(1,N_FAILURE_TYPES)) {
 				relevent_samples = A[,i] == 1
 				W_relevent = W[relevent_samples]
 				df = data.frame(X[relevent_samples,i,])
@@ -95,7 +102,7 @@ CompetingRisksModel$methods(
 			}
 			return(ret)
 		}
-		for (i in seq(1,N_QUESTION)) {
+		for (i in seq(1,N_CLUESTERS_SIZE)) {
 			event_specific_models[[i]] <<- Q_res[[i]]
 			
 		}
@@ -103,18 +110,18 @@ CompetingRisksModel$methods(
   },
 
 get_beta_Z = function(X) {
-	ret = array(rep(0,N*N_QUESTION*N_EVENTS),c(N,N_QUESTION,N_EVENTS))
-	for (i in seq(1,N_EVENTS)) {
-		for (j in seq(1,N_QUESTION)) {
+	ret = array(rep(0,N*N_CLUESTERS_SIZE*N_FAILURE_TYPES),c(N,N_CLUESTERS_SIZE,N_FAILURE_TYPES))
+	for (i in seq(1,N_FAILURE_TYPES)) {
+		for (j in seq(1,N_CLUESTERS_SIZE)) {
 			ret[,j,i] = as.matrix(X[,j,]) %*% coef(event_specific_models[[j]][[i]])
 		}
 	}
 	return(ret)
 },
 get_hazerd_at_event = function(T,D,X,A,W) {
-	ret = array(rep(0,N*N_QUESTION*N_EVENTS),c(N,N_QUESTION,N_EVENTS))
-	for (i in seq(1,N_EVENTS)) {
-		for (j in seq(1,N_QUESTION)) {
+	ret = array(rep(0,N*N_CLUESTERS_SIZE*N_FAILURE_TYPES),c(N,N_CLUESTERS_SIZE,N_FAILURE_TYPES))
+	for (i in seq(1,N_FAILURE_TYPES)) {
+		for (j in seq(1,N_CLUESTERS_SIZE)) {
 			relevent_samples = A[,j] == 1
 			W_ans = W[relevent_samples]
 			res.cox <- event_specific_models[[j]][[i]]
@@ -123,7 +130,7 @@ get_hazerd_at_event = function(T,D,X,A,W) {
 			fit.obj<-coxph.detail(res.cox)
 			clam<-cumsum(fit.obj$hazard[] / as.numeric(exp(t(colSums(matrix(X[relevent_samples,i,])*W_ans) / sum(W_ans)) %*% coef)))
 			clam1<-c(0,clam)
-			stf = stepfun(sort(unique(T[D[,j,i] == 1,j])),clam1)				
+			stf = stepfun(fit.obj$time,clam1)				
 			ret[,j,i] = stf(T[,j])
 		}
 	}
--- example.R
+++ example.R
@@ -1,48 +1,61 @@
 source("competing_risks_model.R")
-
 library(mvtnorm)
 library(abind)
+
+
 set.seed(323214)
-N_QUESTION = 12
-N_EVENTS = 3
-#N_EVENTS = 2
+# Generate dataset
+# Clusetes of size 12 with 3 failure types
+N_CLUESTERS_SIZE = 12
+N_FAILURE_TYPES = 3
 
+#Number of clusters
 N=2000
-wb_k = 1
-wb_g = 1
-MAX_ITR = 150
-P = 0.5
-C = 3
-beta = t(matrix(c(rep(c(1,1.5,0.5),4),rep(c(1.5,1,0.6),4),rep(c(0.75,1,0.5),4)),ncol=N_QUESTION))
 
+#Evert cluster memmber has one covariate with P porbability of bein 1, 0 otherwise
+P = 0.5
+#Censor time
+C = 3
+
+beta = t(matrix(c(rep(c(1,1.5,0.5),4),rep(c(1.5,1,0.6),4),rep(c(0.75,1,0.5),4)),ncol=N_CLUESTERS_SIZE))
+# [ 1., 1.5, 0.5 ]
+#       ...
+# [ 1.5, 1., 0.6 ]
+#       ...
+# [ 0.75, 1, 0.5 ]
+
+
+# Set the varcov of the frailty distreubtion (multivariate normale)
+# [ 1  , 0.2, 0.3 ]
+# [ 0.2, 1. , 0.5 ]
+# [ 0.3, 0.5, 1.  ]
 r12 = 0.2
 r13 = 0.3
 r23 = 0.5
-
 sigma = 1 
 omega_varcov = matrix(c(sigma,r12,r13,r12,sigma,r23,r13,r23,sigma),ncol=3)
 
 
-X = rbinom(N*N_QUESTION, 1, P)
-dim(X) <- c(N,N_QUESTION,1)
+# Sample covariates.
+X = rbinom(N*N_CLUESTERS_SIZE, 1, P)
+dim(X) <- c(N,N_CLUESTERS_SIZE,1)
 
-
+#Sample failure times.
 base_rate = 1
-
 simulate_survial_times <- function(beta,X,n=N,rv,base_rate=1) {
-	proph = exp(t(matrix(rep(beta[,1],n),ncol=n)) * X) * exp(matrix(rep(t(rv),N_QUESTION),ncol=N_QUESTION))
-	T = matrix(rep(0,N*N_QUESTION),nrow=N)
+	proph = exp(t(matrix(rep(beta[,1],n),ncol=n)) * X) * exp(matrix(rep(t(rv),N_CLUESTERS_SIZE),ncol=N_CLUESTERS_SIZE))
+	T = matrix(rep(0,N*N_CLUESTERS_SIZE),nrow=N)
 	for (i in seq(1,N)) {
-		for (j in seq(1,N_QUESTION)) {
+		for (j in seq(1,N_CLUESTERS_SIZE)) {
 			T[i,j] = rexp(base_rate, rate = base_rate*proph[i,j])
 		}
 	}
 	return(T)
 }
 
-
+# Sample frailty.
 sim_RV <- function(n=N,Sigma=omega_varcov) {
-	return(mvtnorm::rmvnorm(n, rep(0,N_EVENTS), Sigma))
+	return(mvtnorm::rmvnorm(n, rep(0,N_FAILURE_TYPES), Sigma))
 }
 
 
@@ -51,6 +64,7 @@ T1 = simulate_survial_times(matrix(beta[,1]),X[,,1],rv=omega[,1],base_rate=1)
 T2 = simulate_survial_times(matrix(beta[,2]),X[,,1],rv=omega[,2],base_rate=1)
 T3 = simulate_survial_times(matrix(beta[,3]),X[,,1],rv=omega[,3],base_rate=1)
 
+#Arrange data to fit interface expectation
 T = pmin(T1,T2,T3,C)
 delta1 = (T == T1)
 delta2 = (T == T2)
@@ -60,5 +74,5 @@ delta= abind(delta1,delta2,delta3,along=3)
 
 source("competing_risks_model.R")
 comprsk = CompetingRisksModel()
-comprsk$fit(T,delta,X)
+comprsk$fit(T,delta,X,ncore=1)
 
